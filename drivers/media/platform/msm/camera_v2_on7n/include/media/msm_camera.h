8d4540667f12435dc9259b82a923211a631883f6
DY3413 <milosix3413@gmail.com>
2023-08-11 17:31:28 +0900

    media: msm: update camera_v2_on7n  - Include header files from G600SKSU1APF4
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- drivers/media/platform/msm/camera_v2_on7n/include/media/msm_camera.h
+++ drivers/media/platform/msm/camera_v2_on7n/include/media/msm_camera.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2009-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -8,39 +8,12 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
  */
+#ifndef __LINUX_MSM_CAMERA_H
+#define __LINUX_MSM_CAMERA_H
 
-#ifndef MSM_CSIPHY_2_2_HWREG_H
-#define MSM_CSIPHY_2_2_HWREG_H
+#include <linux/cdev.h>
+#include "../uapi/media/msm_camera.h"
 
-#include "../msm_csiphy.h"
-
-struct csiphy_reg_parms_t csiphy_v2_2 = {
-	/*MIPI CSI PHY registers*/
-	0x17C,
-	0x0,
-	0x4,
-	0x8,
-	0xC,
-	0x10,
-	0x100,
-	0x104,
-	0x108,
-	0x10C,
-	0x110,
-	0x128,
-	0x140,
-	0x144,
-	0x164,
-	0x180,
-	0x1A0,
-	0x6F,
-	0x1A4,
-	0x1C0,
-	0x1C4,
-	0x4,
-	0x1E0,
-	0x1E8,
-	0x1,
-};
-#endif
+#endif /* __LINUX_MSM_CAMERA_H */
